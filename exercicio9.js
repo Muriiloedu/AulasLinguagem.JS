db1cfd906052b7b35de1c8b766d7ad36ecdae5f3
Murilo Eduardo Lima <161713469+Muriiloedu@users.noreply.github.com>
2025-04-06 15:43:16 -0300

    Delete exercicio9.js

--- exercicio9.js
+++
@@ -1,9 +0,0 @@
-function validarEmail(email) {
-  if (!email.includes("@") || !email.includes('.')) {
-    return false;
-  }
-    return true;
-}
-console.log("validando...");
-console.log(validarEmail("ema@valido.com"));
-console.log(validarEmail("emailinvalido.com"));
